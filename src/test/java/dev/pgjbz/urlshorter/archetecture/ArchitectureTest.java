a949b545a9c42bea14e504db3661bcd88a906687
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2023-01-23 11:55:21 -0300

    test: domain only access domain and java classes

--- src/test/java/dev/pgjbz/urlshorter/archetecture/ArchitectureTest.java
+++ src/test/java/dev/pgjbz/urlshorter/archetecture/ArchitectureTest.java
@@ -1,22 +1,24 @@
 package dev.pgjbz.urlshorter.archetecture;
 
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
-@AnalyzeClasses(packages = "dev.pgjbz.urlshorter", importOptions = ImportOption.DoNotIncludeTests.class)
+@AnalyzeClasses(packages = ArchitectureTest.BASE_PACKAGE, importOptions = ImportOption.DoNotIncludeTests.class)
 class ArchitectureTest {
 
-    private static final String BASE_PACKAGE = "dev.pgjbz.urlshorter";
+    public static final String BASE_PACKAGE = "dev.pgjbz.urlshorter";
     private static final String DOMAIN = "domain";
-    private static final String DOMAIN_PACKAGE = BASE_PACKAGE + ".domain..";
+    private static final String DOMAIN_PACKAGE = "..domain..";
     private static final String INFRA = "infra";
-    private static final String INFRA_PACKAGE = BASE_PACKAGE + ".infra..";
+    private static final String INFRA_PACKAGE = "..infra..";
     private static final String APP = "app";
-    private static final String APP_PACKAGE = BASE_PACKAGE + ".app..";
+    private static final String APP_PACKAGE = "..app..";
+    private static final String JAVA_PACKAGE = "java..";
 
     @ArchTest
     public static final ArchRule shouldBeCleanArch = layeredArchitecture()
@@ -28,4 +30,7 @@ class ArchitectureTest {
             .whereLayer(APP).mayNotBeAccessedByAnyLayer()
             .whereLayer(INFRA).mayOnlyBeAccessedByLayers(APP);
 
+    @ArchTest
+    public static final ArchRule domainOnlyAccessJavaStdLib = classes().that().resideInAPackage(DOMAIN_PACKAGE)
+            .should().onlyAccessClassesThat().resideInAnyPackage(JAVA_PACKAGE, DOMAIN_PACKAGE);
 }
