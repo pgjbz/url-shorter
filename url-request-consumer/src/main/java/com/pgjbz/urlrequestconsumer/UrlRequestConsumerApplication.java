61c09d27009e1ed4a62ee7260fb818fe2df6cac1
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2023-02-22 15:20:18 -0300

    chore: add url-shorter-consumer
    rename shorter to url-shorter-api

---
+++ url-request-consumer/src/main/java/com/pgjbz/urlrequestconsumer/UrlRequestConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.pgjbz.urlrequestconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlRequestConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlRequestConsumerApplication.class, args);
+	}
+
+}
